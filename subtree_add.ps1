37629206f3f065df104cc295f54bdfbf50685b8d
edp1096 <edp1096@naver.com>
2023-06-19 15:00:36 +0900

    Update subtree_add.ps1

--- subtree_add.ps1
+++ subtree_add.ps1
@@ -1,2 +1,2 @@
-# remove-item -r -force -ea 0 faiss
-git subtree add --prefix faiss https://github.com/facebookresearch/faiss.git v1.7.4
+remove-item -r -force -ea 0 faiss
+git subtree add --prefix faiss https://github.com/facebookresearch/faiss.git v1.7.4 --squash
